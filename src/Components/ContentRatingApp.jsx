897851944405c7a8e9e52a1eae980cd0592c3aae
John Villaluna <johnolidv15@gmail.com>
2025-09-24 18:22:16 +0800

    Added some classes with buttons

---
+++ src/Components/ContentRatingApp.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+class ContentRating extends Component {
+    constructor() {
+        super();
+      
+        this.state = {
+            likes: 0,
+            dislikes: 0
+
+            handleLike:() => {
+                this.setState((prevState) => ({
+                    likes: prevState.likes  + 1 
+                }));
+            },
+
+            handleDislike:() => {
+                this.setState((prevState) => ({
+                    dislikes: prevState.dislikes + 1
+                }));
+            }
+
+        };
+    }
+
+    render() {
+        return (
+            <>
+
+
+                
+                <div className = 'content-rating'>
+
+                    <p> -- Add text here-- </p>
+
+                    <div className = 'content-rating'> </div>
+
+                </div>
+
+                <div className = 'content-rating'>
+
+                    <p>
+                        //Add text here. </p>
+
+                    <div className = 'rating-buttons'> </div>
+
+                </div>
+
+                <div className = 'content-rating'>
+
+                    <p> -- Add text here -- </p>
+
+                    <div className = 'rating-buttons'>
+
+                    <button className ='like-button'>
+                        like ({this.state.likes})
+                    </button>
+
+                    <button className = 'dislike-button'>
+                        dislike ({this.state.dislikes})
+                    </button>
+
+                    </div>
+
+                </div>
+
+
+
+            </>
+        );
+    }
+}
+
+export default ContentRating;
